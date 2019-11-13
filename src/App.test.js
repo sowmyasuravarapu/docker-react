1eea4fc5ba76c9aac9dde830419b0d49d01b57eb
root <root@ip-172-31-5-165.ec2.internal>
2019-11-13 14:37:53 +0000

    modified test

--- src/App.test.js
+++ src/App.test.js
@@ -9,9 +9,5 @@ it('renders without crashing', () => {
 });
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
 
