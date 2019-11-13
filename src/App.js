08dde97c94f4d8fbf5155874492c383e04ae456c
root <root@ip-172-31-5-165.ec2.internal>
2019-11-13 14:59:41 +0000

    modified app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         bye there!
+         hello! there!
         </p>
         <a
           className="App-link"
